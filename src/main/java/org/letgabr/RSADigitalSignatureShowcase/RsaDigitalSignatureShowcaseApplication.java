af27ab782eeef087907e7caaa0747d8228fb1f74
Alexey Letunov <xnihonsamuraix@gmail.com>
2024-04-13 13:16:55 +0300

    Init project

---
+++ src/main/java/org/letgabr/RSADigitalSignatureShowcase/RsaDigitalSignatureShowcaseApplication.java
@@ -0,0 +1,13 @@
+package org.letgabr.RSADigitalSignatureShowcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsaDigitalSignatureShowcaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RsaDigitalSignatureShowcaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/letgabr/RSADigitalSignatureShowcase/RsaDigitalSignatureShowcaseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.letgabr.RSADigitalSignatureShowcase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RsaDigitalSignatureShowcaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
